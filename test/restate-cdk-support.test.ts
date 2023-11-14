121e4db7d2a420cfaf93a959f7456f9a913883be
Pavel Tcholakov <pcholakov@gmail.com>
2023-11-14 14:00:40 +0100

    Initial commit

---
+++ test/restate-cdk-support.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RestateCdkSupport from '../lib/restate-cdk-support-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/restate-cdk-support-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RestateCdkSupport.RestateCdkSupportStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
